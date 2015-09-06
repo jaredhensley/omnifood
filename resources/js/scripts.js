dbe1290db8de98471fe86d16d59d7fc1437edacd
Jared Hensley <jahensle85@me.com>
2015-09-06 18:26:14 -0400

    jquery waypoint plugin added and sticky footer conditionals built

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 
-    $('h1').click(function() {
-        $(this).css('background-color', 'blue');
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    /*, {
+      offset: '60px;'*/
+    
+    }, {
+      offset: '60px;'
     });
+    
+    /*var waypoints = $('#handler-first').waypoint(function(direction) {
+      notify(this.element.id + ' hit 25% from top of window') 
+    }, {
+      offset: '25%'
+    })*/
 
 });
