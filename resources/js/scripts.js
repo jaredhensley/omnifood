c9210704d17eb1634cf74feea97be171bbe7c0cd
Jared Hensley <jahensle85@me.com>
2015-09-21 23:36:53 -0400

    update with jquery

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -67,6 +67,22 @@ $(document).ready(function() {
     }, {
         offset: '50%'
     });
+    
+    /*Mobile Navigation*/
+    
+    $('.js--nav-icon').click(function(){
+      var nav = $('.js--main-nav');
+      var icon = $('.js--nav-icon i');
+      nav.slideToggle(200);
+      
+      if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+      } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+      }
+    });
    
 });
 
